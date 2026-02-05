aa977529bdc968a25ea79f0827f4c47b850c579a
Gajendra_Acharya_S <139026399+Gajendra37@users.noreply.github.com>
2026-02-05 20:35:18 +0530

    Create 383.Ransom_Note.c

---
+++ 383.Ransom_Note.c
@@ -0,0 +1,35 @@
+/*
+Description:
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+  
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+  
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+*/
+
+//Solution:
+
+bool canConstruct(char* ransomNote, char* magazine) {
+
+    int count[26] = {0};
+    for(int i=0;magazine[i]!='\0';i++) 
+    {
+        count[magazine[i]-'a']++;
+    }
+    for(int i=0;ransomNote[i]!='\0';i++) 
+    {
+        if(--count[ransomNote[i]-'a']<0)
+            return false;
+    }
+    return true;
+}
