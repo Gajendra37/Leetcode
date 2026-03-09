037874a773013ab728d9222eb9a830f3ebf3860a
Gajendra_Acharya_S <139026399+Gajendra37@users.noreply.github.com>
2026-03-09 15:48:43 +0530

    Update 141.Linked_List_Cycle.c

--- 141.Linked_List_Cycle.c
+++ 141.Linked_List_Cycle.c
@@ -33,7 +33,7 @@ bool hasCycle(struct ListNode *head) {
             fast=fast->next->next;
 
             if(slow==fast)
-            return 1;
+              return true;
         }
-        return 0;
+        return false;
 }
