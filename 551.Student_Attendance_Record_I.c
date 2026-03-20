08bb9f871d6bb5cd0e6e98cfa1e2ae93d843e5cf
Gajendra_Acharya_S <139026399+Gajendra37@users.noreply.github.com>
2026-03-20 09:13:17 +0530

    Create 551.Student_Attendance_Record_I.c

---
+++ 551.Student_Attendance_Record_I.c
@@ -0,0 +1,52 @@
+/*
+Description:
+
+You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day. 
+The record only contains the following three characters:
+'A': Absent.
+'L': Late.
+'P': Present.
+The student is eligible for an attendance award if they meet both of the following criteria:
+The student was absent ('A') for strictly fewer than 2 days total.
+The student was never late ('L') for 3 or more consecutive days.
+Return true if the student is eligible for an attendance award, or false otherwise.
+
+Example 1:
+Input: s = "PPALLP"
+Output: true
+Explanation: The student has fewer than 2 absences and was never late 3 or more consecutive days.
+
+Example 2:
+Input: s = "PPALLL"
+Output: false
+Explanation: The student was late 3 consecutive days in the last 3 days, so is not eligible for the award.
+*/
+
+//Solution:
+
+bool checkRecord(char* s) {
+    
+    int a=0,l=0,cons=0;
+    int len=strlen(s);
+    for(int i=0;i<len;i++)
+    {
+        char ch=s[i];
+        if(ch=='A')
+        {
+            a++;
+            l=0;
+        }
+        else if(ch=='P')
+            l=0;
+        else if(ch=='L')
+        {
+            l++;
+            if(cons<l)
+                cons=l;
+        }
+    }
+    if(a<2 && cons<3)
+        return true;
+    else 
+        return false;
+}
