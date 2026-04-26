610ae76d53071f88719c4ce702c459acdac17e42
Gajendra_Acharya_S <139026399+Gajendra37@users.noreply.github.com>
2026-04-26 17:42:12 +0530

    Create 434.Number_of_Segments_in_a_String.c

---
+++ 434.Number_of_Segments_in_a_String.c
@@ -0,0 +1,28 @@
+/*
+Description:
+
+Given a string s, return the number of segments in the string.
+A segment is defined to be a contiguous sequence of non-space characters.
+
+Example 1:
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+
+Example 2:
+Input: s = "Hello"
+Output: 1
+*/
+
+//Solution:
+
+int countSegments(char* s) {
+    
+    int count=0;
+    for(int i=0;s[i];i++)
+    {
+        if(s[i]!=' ' && (i==0 || s[i-1]==' '))
+            count++;
+    }
+    return count;
+}
