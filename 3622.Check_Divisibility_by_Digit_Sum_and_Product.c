e0a419bb3d01276ee61dbe1ca86fb09d765362d1
Gajendra_Acharya_S <139026399+Gajendra37@users.noreply.github.com>
2026-08-22 21:18:16 +0530

    Create 3622.Check_Divisibility_by_Digit_Sum_and_Product.c

---
+++ 3622.Check_Divisibility_by_Digit_Sum_and_Product.c
@@ -0,0 +1,38 @@
+/*
+Description:
+
+You are given a positive integer n. Determine whether n is divisible by the sum of the following two values:
+The digit sum of n (the sum of its digits).
+The digit product of n (the product of its digits).
+Return true if n is divisible by this sum; otherwise, return false.
+
+Example 1:
+Input: n = 99
+Output: true
+Explanation:
+Since 99 is divisible by the sum (9 + 9 = 18) plus product (9 * 9 = 81) of its digits (total 99), the output is true.
+
+Example 2:
+Input: n = 23
+Output: false
+Explanation:
+Since 23 is not divisible by the sum (2 + 3 = 5) plus product (2 * 3 = 6) of its digits (total 11), the output is false.
+*/
+
+//Solution:
+
+bool checkDivisibility(int n) {
+    
+    int temp=n,pdt=1,sum=0;
+    while(temp>0)
+    {
+        pdt*=temp%10;
+        sum+=temp%10;
+        temp/=10;
+    }
+    if(n%(sum+pdt)==0)
+        return true;
+    else
+        return false;
+
+}
